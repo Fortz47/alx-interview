d49e79f5b36d6f8516d0f3ba29959cab3fef8eff
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-08 21:23:05 +0000

    solved pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""creates a Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """returns a list of integers representing
+    the Pascal's triangle of n"""
+    if n <= 0:
+        return []
+
+    res = [[] for i in range(n)]
+    res[0].append(1)
+    for i in range(1, n):
+        for k in range(0, i + 1):
+            val = 1 if k == 0 or k == i else res[i - 1][k - 1] + res[i - 1][k]
+            res[i].append(val)
+    return res
