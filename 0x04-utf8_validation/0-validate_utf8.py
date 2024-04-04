f33c134b26e21f19a80b726983eed7f607961eb1
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-04 23:56:55 +0100

    Updated 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,7 @@ def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
     if all(isinstance(x, int) for x in data) and isinstance(data, list):
         if len(data):
-            for val in data:
-                if not (val >= 0 and val <= 127):
-                    return False
+            if not (data[0] >= 0 and data[0] <= 127):
+                return False
             return True
     return False
