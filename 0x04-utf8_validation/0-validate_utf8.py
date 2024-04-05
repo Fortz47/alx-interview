33359b54a146bdda3e4eaccf54ccb51ad2524764
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-05 01:07:33 +0100

    Update validation logic

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""determines if a given data set represents a valid UTF-8 encoding"""
+"""UTF-8 encoding Validation Module"""
+from typing import List
 
 
-def validUTF8(data):
+def validUTF8(data: List) -> bool:
     """determines if a given data set represents a valid UTF-8 encoding"""
-    dummy = data[0] >> 7
-    if all(isinstance(x, int) for x in data) and isinstance(data, list):
-        if len(data):
-            for val in data:
-                if not (val >= 0 and val <= 255):
-                    return False
-            return True
-    return False
+    num_bytes_to_follow = 0
+
+    for byte in data:
+        if num_bytes_to_follow == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes_to_follow = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes_to_follow = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes_to_follow = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes_to_follow -= 1
+
+    return num_bytes_to_follow == 0
