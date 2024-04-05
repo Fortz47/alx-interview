ca78c11ab39cc53fe4c20a8342689c98c39efe9e
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-05 01:19:43 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,19 +3,21 @@
 from typing import List
 
 
-def validUTF8(data: List) -> bool:
+def validUTF8(data: List[int]) -> bool:
     """determines if a given data set represents a valid UTF-8 encoding"""
     num_bytes_to_follow = 0
 
     for byte in data:
         if num_bytes_to_follow == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 7) == 0:
+                continue
+            elif (byte >> 5) == 0b110:
                 num_bytes_to_follow = 1
             elif (byte >> 4) == 0b1110:
                 num_bytes_to_follow = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes_to_follow = 3
-            elif (byte >> 7):
+            else:
                 return False
         else:
             if (byte >> 6) != 0b10:
