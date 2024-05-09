922930aa7eabd527ff3c5d81f710dc6f5dd5f6f6
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-09 18:54:58 +0000

    solved island perimeter coding challenge

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Island perimeter grid Module"""
+
+def island_perimeter(grid):
+    """
+     returns the perimeter of the island described in grid:
+
+     grid is a list of list of integers:
+        0 represents water
+        1 represents land
+        Each cell is square, with a side length of 1
+        Cells are connected horizontally/vertically (not diagonally).
+        grid is rectangular, with its width and height not exceeding 100
+        
+    The grid is completely surrounded by water
+    There is only one island (or nothing).
+    The island doesn’t have “lakes” (water inside that isn’t connected
+    to the water surrounding the island).
+    """
+
+    if not isinstance(grid, list):
+        return
+    perimeter = 0
+    grid_height = len(grid)
+    grid_width = len(grid[0])
+    if grid_height > 100 or grid_width > 100
+        return
+
+    for row in range(grid_height):
+        for cell in range(grid_width):
+            if grid[row][cell] != 1:
+                continue
+            right = 0 if grid[row][cell + 1] else 1
+            left = 0 if grid[row][cell - 1] else 1
+            up = 0 if grid[row - 1][cell] else 1
+            down = 0 if grid[row + 1][cell] else 1
+            perimeter += (right + left + up + down)
+    return perimeter
